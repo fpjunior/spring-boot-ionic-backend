4beb1b41c82ed26dca46c88e3dee6b6b2c302bfa
fpjunior <fpjuniorpx@gmail.com>
2020-01-21 09:36:07 -0300

    Criar classe Categorias

--- src/main/java/com/fpedro/cursomc/domain/Categoria.java
+++ src/main/java/com/fpedro/cursomc/domain/Categoria.java
@@ -2,11 +2,19 @@ package com.fpedro.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
- static final long serialVersionUID = 1L;
- 
-private Integer id;	
- private String nome;
+ private static final long serialVersionUID = 1L;
+	 
+ 	 @Id
+	 @GeneratedValue(strategy=GenerationType.IDENTITY)
+	 private Integer id;
+	 private String nome;
  
  public Categoria () {
 	 		
