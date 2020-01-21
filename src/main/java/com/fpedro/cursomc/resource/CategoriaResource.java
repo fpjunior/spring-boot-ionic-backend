2349d9721d3c41d730a3606d841a012d3e489838
fpjunior <fpjuniorpx@gmail.com>
2020-01-21 10:23:57 -0300

    criar services

--- src/main/java/com/fpedro/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/fpedro/cursomc/resource/CategoriaResource.java
@@ -1,28 +1,25 @@
 package com.fpedro.cursomc.resource;
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fpedro.cursomc.domain.Categoria;
+import com.fpedro.cursomc.services.CategoriaService;
+
 @RestController
 @RequestMapping(value="/categorias")
-
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
  
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-			Categoria cat1 = new Categoria(1, "Informatica");
-			Categoria cat2 = new Categoria(2, "escritório");
-			
-			List<Categoria> lista = new ArrayList<>();
-			
-			lista.add(cat1);
-			lista.add(cat2);
-					return lista;
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+			Categoria obj = service.buscar(id);
+			return ResponseEntity.ok().body(obj);		
 			
 	}
 }
