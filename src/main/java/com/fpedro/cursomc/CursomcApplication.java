39fd1e7e78780121ecbc2b6a1f4bfb39540d14f6
fpjunior <fpjuniorpx@gmail.com>
2020-02-06 12:16:41 -0300

    criar class cidade e estado

--- src/main/java/com/fpedro/cursomc/CursomcApplication.java
+++ src/main/java/com/fpedro/cursomc/CursomcApplication.java
@@ -3,8 +3,12 @@ package com.fpedro.cursomc;
 import java.util.Arrays;
 
 import com.fpedro.cursomc.domain.Categoria;
+import com.fpedro.cursomc.domain.Cidade;
+import com.fpedro.cursomc.domain.Estado;
 import com.fpedro.cursomc.domain.Usuario;
 import com.fpedro.cursomc.repositories.CategoriaRepository;
+import com.fpedro.cursomc.repositories.CidadeRepository;
+import com.fpedro.cursomc.repositories.EstadoRepository;
 import com.fpedro.cursomc.repositories.UsuarioRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,39 +18,58 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
 	@Autowired
-	private CategoriaRepository	categoriaRepository;
+	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Motorista");
 		Categoria cat2 = new Categoria(null, "Carona");
-		
+
 		Usuario p1 = new Usuario(null, "Fernando", 33);
-		Usuario p2 = new Usuario(null, "Renato", 32 );
+		Usuario p2 = new Usuario(null, "Renato", 32);
 		Usuario p3 = new Usuario(null, "Andreson", 35);
 		Usuario p4 = new Usuario(null, "Arthur", 25);
-		
+
 		cat1.getUsuarios().addAll(Arrays.asList(p1, p4));
 		cat2.getUsuarios().addAll(Arrays.asList(p1, p2, p3, p4));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p2.getCategorias().addAll(Arrays.asList(cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat2));
 		p4.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		
+
+		Estado est1 = new Estado(null, "Pernambuco");
+		Estado est2 = new Estado(null, "Parabíba");
+		Estado est3 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Carpina", est1);
+		Cidade c2 = new Cidade(null, "João Pessoa", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est3);
+		Cidade c4 = new Cidade(null, "Recife", est1);
+
+		est1.getCidades().addAll(Arrays.asList(c1, c4));
+		est2.getCidades().addAll(Arrays.asList(c2));
+		est3.getCidades().addAll(Arrays.asList(c3));
+
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		usuarioRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
-		
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2, est3));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3, c4));
+
 	}
 
 }
-
---
+++ src/main/java/com/fpedro/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.fpedro.cursomc.repositories;
+
+import com.fpedro.cursomc.domain.Estado;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
