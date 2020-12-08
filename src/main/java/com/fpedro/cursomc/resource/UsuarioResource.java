2e445d019860be8a6f71dabff0d33cec74a97f03
fpjunior <fpjuniorpx@gmail.com>
2020-12-08 19:46:54 -0300

    crie recurso post em usuario

--- src/main/java/com/fpedro/cursomc/resource/UsuarioResource.java
+++ src/main/java/com/fpedro/cursomc/resource/UsuarioResource.java
@@ -1,5 +1,6 @@
 package com.fpedro.cursomc.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fpedro.cursomc.domain.Usuario;
 import com.fpedro.cursomc.dto.UsuarioDTO;
@@ -21,26 +23,34 @@ import com.fpedro.cursomc.services.UsuarioService;
 public class UsuarioResource {
 	
 	@Autowired
-	private UsuarioService service;
+	private UsuarioService usuarioService;
  
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Usuario> find(@PathVariable Integer id) {
-			Usuario obj = service.find(id);
+			Usuario obj = usuarioService.find(id);
 			return ResponseEntity.ok().body(obj);	
 				
 	}
 
 	public ResponseEntity<Void> update(@RequestBody UsuarioDTO objDto, @PathVariable Integer id){
-		Usuario obj = service.fromDTO(objDto);
+		Usuario obj = usuarioService.fromDTO(objDto);
 		obj.setId(id);
-		obj = service.update(obj);
+		obj = usuarioService.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@RequestMapping( method = RequestMethod.GET)
 	public ResponseEntity <List<Usuario>> findAll() {
-		List<Usuario> list = service.findAll();
+		List<Usuario> list = usuarioService.findAll();
 		return ResponseEntity.ok().body(list);
 
+	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Usuario obj) {
+		obj = usuarioService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+		.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
